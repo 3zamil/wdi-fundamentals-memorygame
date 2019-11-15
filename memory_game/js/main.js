7809b05789a812752719ffdf73070892c313c2ab
Abdullah <3zamil@gmail.com>
2019-11-15 21:04:35 +0300

    final project

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -37,8 +37,8 @@ function checkForMatch(){
 		}
 }
 
-function flipCard(cardId) {
-
+function flipCard() {
+	cardId = this.getAttribute('data-id');
 /*	var cardOne = cards[0];
 	cardsInPlay.push(cardOne);
 	console.log("User flipped " + cardsInPlay[0]);
@@ -53,11 +53,24 @@ function flipCard(cardId) {
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
 
+		this.setAttribute("src", cards[cardId].cardImage);
+
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
 
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+		
+	}
+}
+createBoard();
